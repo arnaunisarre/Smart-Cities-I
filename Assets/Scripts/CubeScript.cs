52aa59514df826944e6a7e37edf8421c73bd3d69
arnaunisarre <arnau.nisarre@estudiant.upc.edu>
2019-03-11 07:05:53 +0100

    First commit
    First commit

---
+++ Assets/Scripts/CubeScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeScript : MonoBehaviour
+{
+    [SerializeField]
+    float lat;
+    [SerializeField]
+    float lon;
+
+    private float latgps;
+    private float longps;
+
+    private GameObject gpsPointer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gpsPointer = GameObject.Find("GpsHandler");
+        latgps = gpsPointer.GetComponent<GpsHandler>().latUser;
+        longps = gpsPointer.GetComponent<GpsHandler>().lonUser;
+        this.gameObject.GetComponent<Renderer>().enabled = false;
+
+        StartCoroutine(HideShow());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        
+    }
+
+    public IEnumerator HideShow()
+    {
+        yield return new WaitForSeconds(5.0f);
+        if (CalcDistance(lat, lon, latgps, longps) <= 20.0f)
+        {
+            this.gameObject.GetComponent<Renderer>().enabled = true;
+
+        }
+
+
+    }
+
+    public float CalcDistance(float lat1,float lon1, float lat2, float lon2)
+    {
+        return 10.0f;
+    }
+
+}
---
+++ Assets/Scripts/GpsHandler.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GpsHandler : MonoBehaviour
+{
+    [SerializeField]
+    Text latitudeText;
+
+    [SerializeField]
+    Text longitudeText;
+
+    public float latUser;
+    public float lonUser;
+
+    IEnumerator Start()
+    {
+
+        latUser = 0.0f;
+        lonUser = 0.0f;
+
+        // First, check if user has location service enabled
+        if (!Input.location.isEnabledByUser)
+            yield break;
+
+        // Start service before querying location
+        Input.location.Start();
+
+        // Wait until service initializes
+        int maxWait = 20;
+        while (Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        // Service didn't initialize in 20 seconds
+        if (maxWait < 1)
+        {
+            print("Timed out");
+            yield break;
+        }
+
+        // Connection has failed
+        if (Input.location.status == LocationServiceStatus.Failed)
+        {
+            print("Unable to determine device location");
+            yield break;
+        }
+        else
+        {
+            // Access granted and location value could be retrieved
+            print("Location: " + Input.location.lastData.latitude + " " + Input.location.lastData.longitude + " " + Input.location.lastData.altitude + " " + Input.location.lastData.horizontalAccuracy + " " + Input.location.lastData.timestamp);
+        }
+
+        // Stop service if there is no need to query location updates continuously
+        //Input.location.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        latitudeText.GetComponent<Text>().text = Input.location.lastData.latitude.ToString();
+
+        latUser = Input.location.lastData.latitude;
+
+        longitudeText.GetComponent<Text>().text = Input.location.lastData.longitude.ToString();
+
+        lonUser = Input.location.lastData.longitude;
+
+
+    }
+}
